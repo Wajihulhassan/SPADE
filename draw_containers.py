487cdbd0e3593cd0c3735a492cb78de88588279e
Wajihulhassan <wajih.lums@gmail.com>
2017-01-25 14:51:23 -0500

    adding option to separate specific container graph

--- draw_containers.py
+++ draw_containers.py
@@ -4,11 +4,13 @@ import sys
 import random
 from collections import defaultdict
 flag = False
+get_cont = False
 if len(sys.argv) < 3:
     print("provide more arguments")
 
 if len(sys.argv) == 4:
-    flag = True
+    get_cont = True
+    cont_id = sys.argv[3]
 
 lines = []
 lines_with_edges = []
@@ -29,15 +31,6 @@ with open(sys.argv[1],'r') as fil:
         else:
             lines_without_edges.append(l)
 
-if flag:
-    with open(sys.argv[3],'r') as fil:
-        for l in fil:
-            if '->' in l:
-                lines_with_edges.append(l)
-            else:
-                lines_without_edges.append(l)
-
-
 for line in lines_without_edges:
     if 'Cont_ID' in line:
         tokens = line.split('\\n')
@@ -47,20 +40,37 @@ for line in lines_without_edges:
     elif 'subtype:network' in line:
         lines_without_contid.append(line)
 
+cont_map = defaultdict(list)
+
+for key, val in nodes_map.items():
+    for l in val:
+        tokens = l.split(" ")
+        cont_map[key].append(tokens[0])
+
 with open(sys.argv[2], 'w') as fil:
     fil.write(start)
     for l in lines_without_contid:
         fil.write(l)
     for key, val in nodes_map.items():
+        if get_cont and key not in cont_id:
+            continue
         color = [round(random.random(), 5) for x in range(0,3)]
         r = lambda: random.randint(0,255)
         hex_str = '#%02X%02X%02X' % (r(),r(),r())
         subgraph_start = "subgraph cluster_%s { label=\"UUID = %s\" color=\"%s86\" style=\"filled,dashed\" ;\n" % (str(key), str(key),hex_str)
-        
         fil.write('\n' + subgraph_start + '\n')
         for l in val:
             fil.write(l)
         fil.write(end_bracket+'\n')
     for l in lines_with_edges:
-        fil.write(l)
+        tokens = l.split(" ")
+        node1 = tokens[0]
+        node2 = tokens[2]
+        if get_cont:
+            list_nodes = cont_map[cont_id]
+            if node1 in list_nodes or node2 in list_nodes:
+                fil.write(l)
+        else:
+            fil.write(l)
     fil.write(end_bracket+'\n')
+
