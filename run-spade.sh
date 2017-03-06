0c87479011bca978822e5f0297b5710c37d70338
Wajih Ul Hassan <wajih.lums@gmail.com>
2017-03-06 23:12:13 +0000

    fixes from ec2

--- run-spade.sh
+++ run-spade.sh
@@ -6,4 +6,5 @@ sudo /bin/rm -f /var/log/audit/audit.log
 sudo service auditd restart 
 /bin/rm -f /tmp/provenance.dot 
 /bin/rm -f /tmp/audit.log 
+/bin/rm -rf /tmp/spade.graph_db
 ./bin/spade start
